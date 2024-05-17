724361fcd30dddbe002923ac1db5f52f06779eee
Dauren <zhylkybaevdauren@gmail.com>
2024-05-17 10:37:47 +0500

    upd

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createRoot } from 'react-dom/client';
-import UserStore from './store/UserStore'
-import DevicePage from './pages/DevicePage';
-import './styles/reset.css';
-import './styles/common.css';
-
+import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
 
 export const Context = createContext(null)
+
 ReactDOM.render(
-  <Context.Provider value={{
-      user: new UserStore(),
-      device: new DevicePage(),
+    <Context.Provider value={{
+        user: new UserStore(),
+        device: new DeviceStore(),
     }}>
-    <App />
-  </Context.Provider>,
+        <App />
+    </Context.Provider>,
   document.getElementById('root')
 );
